d4c4042cf2b7dd655abefd94f4ad60b54813291d
goblingift <andre.kessler91@gmx.de>
2019-06-23 14:00:02 +0200

    Added weight-measurement

--- Pi4JTestApplication/src/main/java/gift/goblin/pi4jtestapplication/MainClass.java
+++ Pi4JTestApplication/src/main/java/gift/goblin/pi4jtestapplication/MainClass.java
@@ -13,44 +13,41 @@ import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.gpio.trigger.GpioSetStateTrigger;
+import com.pi4j.wiringpi.GpioUtil;
 
 /**
  *
  * @author andre
  */
 public class MainClass {
-    
+
     public static void main(String[] args) throws InterruptedException {
-        
+
         System.out.println("Started application!");
-        
-        
-        // create gpio controller
-        final GpioController gpio = GpioFactory.getInstance();
 
-        // provision gpio pin #3 (Pin 22 BCM) as an output pin
-        final GpioPinDigitalOutput pinLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "Blue-LED", PinState.LOW);
-
-        // set input pin #00 (Pin 17 BCM)
-        GpioPinDigitalInput inputPin = gpio.provisionDigitalInputPin(RaspiPin.GPIO_00, PinPullResistance.PULL_DOWN);
-        inputPin.setShutdownOptions(true);
-        
-        // create a gpio control trigger on the input pin ; when the input goes HIGH, also set gpio pin #03 to High and vice versa
-        inputPin.addTrigger(new GpioSetStateTrigger(PinState.HIGH, pinLed, PinState.HIGH));
-        inputPin.addTrigger(new GpioSetStateTrigger(PinState.LOW, pinLed, PinState.LOW));
-        
-        
-        
-        
-        System.out.println("Run program for 1 more minute, then terminate!");
-        Thread.sleep(60_000);
-        
-        // stop all GPIO activity/threads by shutting down the GPIO controller
-        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
-        gpio.shutdown();
+        new MainClass().doWork();
 
         System.out.println("Shut down application!");
-        
     }
-    
+
+    public void doWork() {
+
+        final GpioPinDigitalInput pinHXDAT;
+        final GpioPinDigitalOutput pinHXCLK;
+        final HX711 hx;
+
+        GpioUtil.enableNonPrivilegedAccess();
+
+        GpioController gpio = GpioFactory.getInstance();
+        pinHXDAT = gpio.provisionDigitalInputPin(RaspiPin.GPIO_15, "HX_DAT", PinPullResistance.OFF);
+        pinHXCLK = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_16, "HX_CLK", PinState.LOW);
+
+        hx = new HX711(pinHXDAT, pinHXCLK, 128);
+
+        hx.read();
+        System.out.println(hx.value);
+        System.out.println(hx.weight);
+
+    }
+
 }
