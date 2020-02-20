a78a19ef81093b7abc17219b14ece89106f230e6
André Keßler <andre.kessler91@gmx.de>
2020-02-20 20:01:44 +0100

    Improved test application

--- Pi4JTestApplication/src/main/java/gift/goblin/pi4jtestapplication/MainClass.java
+++ Pi4JTestApplication/src/main/java/gift/goblin/pi4jtestapplication/MainClass.java
@@ -16,6 +16,8 @@ import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import com.pi4j.io.gpio.trigger.GpioCallbackTrigger;
 import com.pi4j.wiringpi.GpioUtil;
+import gift.goblin.hx711.GainFactor;
+import gift.goblin.hx711.Hx711;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,8 +27,31 @@ import java.util.logging.Logger;
  */
 public class MainClass {
 
-    private static final int PIN_NO_BUTTON_MAINTENANCE = 1;
-    private static final int PIN_NO_BUTTON_TARE = 2;
+    private static final int PIN_NO_LOAD_CELL_1_DAT = 15;
+    private static final int PIN_NO_LOAD_CELL_1_SCK = 16;
+    private static final int PIN_NO_LOAD_CELL_2_DAT = 4;
+    private static final int PIN_NO_LOAD_CELL_2_SCK = 5;
+    private static final int PIN_NO_LOAD_CELL_3_DAT = 6;
+    private static final int PIN_NO_LOAD_CELL_3_SCK = 10;
+    private static final int PIN_NO_LOAD_CELL_4_DAT = 11;
+    private static final int PIN_NO_LOAD_CELL_4_SCK = 31;
+    
+    // Pins for the load cells
+    private GpioPinDigitalInput pinLoadCell1Dat;
+    private GpioPinDigitalOutput pinLoadCell1Sck;
+    private GpioPinDigitalInput pinLoadCell2Dat;
+    private GpioPinDigitalOutput pinLoadCell2Sck;
+    private GpioPinDigitalInput pinLoadCell3Dat;
+    private GpioPinDigitalOutput pinLoadCell3Sck;
+    private GpioPinDigitalInput pinLoadCell4Dat;
+    private GpioPinDigitalOutput pinLoadCell4Sck;
+    
+    private Hx711 hx711LoadCell1;
+    private Hx711 hx711LoadCell2;
+    private Hx711 hx711LoadCell3;
+    private Hx711 hx711LoadCell4;
+    
+    GpioController gpioController = GpioFactory.getInstance();
 
     public static void main(String[] args) throws InterruptedException {
 
@@ -36,67 +61,51 @@ public class MainClass {
 
         System.out.println("Shut down application!");
     }
+    
+    private void initializeSensors() {
+        pinLoadCell1Dat = gpioController.provisionDigitalInputPin(RaspiPin.getPinByAddress(PIN_NO_LOAD_CELL_1_DAT),
+                "Load-cell 1 DAT", PinPullResistance.OFF);
+        pinLoadCell1Sck = gpioController.provisionDigitalOutputPin(RaspiPin.getPinByAddress(PIN_NO_LOAD_CELL_1_SCK),
+                "Load-cell 1 SCK", PinState.LOW);
+        hx711LoadCell1 = new Hx711(pinLoadCell1Dat, pinLoadCell1Sck, 500, 2.0, GainFactor.GAIN_128);
 
+        pinLoadCell2Dat = gpioController.provisionDigitalInputPin(RaspiPin.getPinByAddress(PIN_NO_LOAD_CELL_2_DAT),
+                "Load-cell 2 DAT", PinPullResistance.OFF);
+        pinLoadCell2Sck = gpioController.provisionDigitalOutputPin(RaspiPin.getPinByAddress(PIN_NO_LOAD_CELL_2_SCK),
+                "Load-cell 2 SCK", PinState.LOW);
+        hx711LoadCell2 = new Hx711(pinLoadCell2Dat, pinLoadCell2Sck, 500, 2.0, GainFactor.GAIN_128);
+
+        pinLoadCell3Dat = gpioController.provisionDigitalInputPin(RaspiPin.getPinByAddress(PIN_NO_LOAD_CELL_3_DAT),
+                "Load-cell 3 DAT", PinPullResistance.OFF);
+        pinLoadCell3Sck = gpioController.provisionDigitalOutputPin(RaspiPin.getPinByAddress(PIN_NO_LOAD_CELL_3_SCK),
+                "Load-cell 3 SCK", PinState.LOW);
+        hx711LoadCell3 = new Hx711(pinLoadCell3Dat, pinLoadCell3Sck, 500, 2.0, GainFactor.GAIN_128);
+
+        pinLoadCell4Dat = gpioController.provisionDigitalInputPin(RaspiPin.getPinByAddress(PIN_NO_LOAD_CELL_4_DAT),
+                "Load-cell 4 DAT", PinPullResistance.OFF);
+        pinLoadCell4Sck = gpioController.provisionDigitalOutputPin(RaspiPin.getPinByAddress(PIN_NO_LOAD_CELL_4_SCK),
+                "Load-cell 4 SCK", PinState.LOW);
+        hx711LoadCell4 = new Hx711(pinLoadCell4Dat, pinLoadCell4Sck, 500, 2.0, GainFactor.GAIN_128);
+    }
+
+    // endless worker
     public void doWork() throws InterruptedException {
-
-        final GpioPinDigitalInput pinHXDAT;
-        final GpioPinDigitalOutput pinHXCLK;
-        final HX711 hx;
-
-        GpioUtil.enableNonPrivilegedAccess();
-
-        GpioController gpio = GpioFactory.getInstance();
-
-        GpioPinDigitalInput pinMaintenance = gpio.provisionDigitalInputPin(RaspiPin.getPinByAddress(PIN_NO_BUTTON_MAINTENANCE),
-                "Button maintenance", PinPullResistance.PULL_DOWN);
-        
-        GpioPinDigitalInput pinTare = gpio.provisionDigitalInputPin(RaspiPin.getPinByAddress(PIN_NO_BUTTON_TARE),
-                "Button tare", PinPullResistance.PULL_DOWN);
-        
-        
-        pinMaintenance.addTrigger(new GpioCallbackTrigger(PinState.HIGH,
-                new MaintenanceTrigger(pinMaintenance)));
-        
-        pinTare.addTrigger(new GpioCallbackTrigger(PinState.HIGH,
-                new MaintenanceTrigger(pinTare)));
-        
-//        pinMaintenance.addListener(new GpioPinListenerDigital() {
-//            @Override
-//            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-//                // display pin state on console
-//                System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
-//
-//                // If button is pressed, start measuring how long
-//                if (event.getState() == PinState.HIGH) {
-//
-//                    long duration;
-//                    for (duration = 0; duration < 3000; duration += 50) {
-//                        if (event.getState() == PinState.LOW) {
-//                            break;
-//                        } else {
-//                            try {
-//                                Thread.sleep(50);
-//                            } catch (InterruptedException ex) {
-//                                Logger.getLogger(MainClass.class.getName()).log(Level.WARNING, null, ex);
-//                            }
-//                        }
-//                    }
-//
-//                    if (duration >= 3000) {
-//                        System.out.println("Button was pressed at least 3s");
-//                    } else {
-//                        System.out.println("Button was pressed only mS: " + duration);
-//                    }
-//
-//                }
-//
-//            }
-//
-//        }
-//        );
-
         while (true) {
-            Thread.sleep(50);
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            // sleep 1 minute
+            Thread.sleep(1000 * 60);
         }
     }
 
